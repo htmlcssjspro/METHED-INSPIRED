cfd5c5de22c564fd25ef34d874cc02d92ffbfc2a
Sergei MILITER <militer@htmlcssjs.pro>
2023-01-20 23:11:39 +1000

    CHANGE port 3001

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,6 +1,7 @@
 const protocol = window.location.protocol;
 const hostname = window.location.hostname;
-export const API_HOST = `${protocol}//${hostname}:8024`;
+const port = '3001';
+export const API_HOST = `${protocol}//${hostname}:${port}`;
 export const API_URL = `${API_HOST}/api`;
 export const PAGINATION_COUNT = 3;
 export const DATA = {};
